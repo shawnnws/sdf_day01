e44bcdc736bcabedd9c4a3c9a3bffb3123acdb7a
shawnnws <shawnngweisong@gmail.com>
2023-04-17 12:14:27 +0800

    first commit

---
+++ src/main/java/nus/iss/sdf/App.java
@@ -0,0 +1,13 @@
+package nus.iss.sdf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
